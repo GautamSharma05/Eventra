517bfbae4f7b800ba6e0f63bdce151ae01200652
Gautam Sharma <gautam@zibtek.in>
2024-04-21 15:49:43 +0530

    Add latestMedia and venues data to LatestMedia and PopularVenues components

--- src/components/Latest Media/LatestMedia.tsx
+++ src/components/Latest Media/LatestMedia.tsx
@@ -5,7 +5,34 @@ import {
   CardFooter,
  
 } from "@/components/ui/card";
-
+const latestMedia = [
+  {
+    id: 1,
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, voluptates.",
+    image: "https://image.wedmegood.com/resized/450X/uploads/member/827689/1603689033_SPP_5397.JPG",
+  },
+  {
+    id: 2,
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, voluptates.",
+    image: "https://image.wedmegood.com/resized/450X/uploads/member/827689/1603689038_SPP_5380.JPG",
+  },
+  {
+    id: 3,
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, voluptates.",
+    image: "	https://image.wedmegood.com/resized/450X/uploads/member/827689/1598705614_IMG_6160.JPG",
+  },
+  {
+    id: 4,
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, voluptates.",
+    image: "https://image.wedmegood.com/resized/450X/uploads/member/827689/1598705629_IMG_6161.JPG",
+  },
+  {
+    id: 5,
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, voluptates.",
+    image: "https://image.wedmegood.com/resized/450X/uploads/member/827689/1598705599_IMG_6169.JPG",
+  },
+  
+]
 export default function LatestMedia() {
   return (
     <div>
@@ -14,13 +41,13 @@ export default function LatestMedia() {
         <h5 className="px-4 pt-4 text-xs">view all(10)</h5>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-5 2xl:grid-cols-8 gap-4 p-4">
-        {[1, 2, 3, 4,5].map((item) => (
-          <Card className="md:w-[252px]" key={item}>
+        {latestMedia.map((item:any) => (
+          <Card className="md:w-[252px]" key={item.id}>
             <CardContent className="p-4 flex justify-center items-center">
               <img
                 width={150}
                 height={150}
-                src="https://via.placeholder.com/150"
+                src={item.image}
                 alt="media"
               />
             </CardContent>
--- src/components/Popular Venues/PopularVenues.tsx
+++ src/components/Popular Venues/PopularVenues.tsx
@@ -1,5 +1,48 @@
 import { Card, CardContent, CardDescription, CardFooter } from "../ui/card";
+const venues = [
+  {
+      id: 1,
+      title: "Hotel Raj Bagh Palace Jaipur, road, Amer, Jaipur, Rajasthan, India",
+      image: "https://image.wedmegood.com/resized/450X/uploads/member/827689/1573028225_Screenshot_from_2019_11_06_13_43_54.png",
+  },
+  {
+      id:2,
+      title: "Khasa Kothi Flyover, D Circle, Gopalbari, Jaipur, Rajasthan 302001",
+      image: "https://image.wedmegood.com/resized/1000X/uploads/member/590315/1558587210_1.jpg",
+  },
 
+  {
+    id: 3,
+    title: "Zorawer Singh gate, Amer road, Jaipur 302002 (India)",
+    image: "https://image.wedmegood.com/resized/1000X/uploads/member/780488/1627046807_WhatsApp_Image_2021_07_23_at_14.01.27__38_.jpeg?crop=7,94,1267,712",
+  },
+  {
+    id:4,
+    title: "District Jaipur, Chomu, Rajasthan 303702",
+    image: "https://image.wedmegood.com/resized/450X/uploads/member/827689/1573028225_Screenshot_from_2019_11_06_13_43_54.png",
+  },
+  {
+    id:5,
+    title: "Hotel Raj Bagh Palace Jaipur",
+    image: "https://image.wedmegood.com/resized/450X/uploads/member/827689/1573028225_Screenshot_from_2019_11_06_13_43_54.png",
+  },
+  {
+    id:6,
+    title: "Hotel Raj Bagh Palace Jaipur",
+    image: "https://image.wedmegood.com/resized/450X/uploads/member/827689/1573028225_Screenshot_from_2019_11_06_13_43_54.png",
+  },
+  {
+    id:7,
+    title: "Hotel Raj Bagh Palace Jaipur",
+    image: "https://image.wedmegood.com/resized/450X/uploads/member/827689/1573028225_Screenshot_from_2019_11_06_13_43_54.png",
+  },
+  {
+    id:8,
+    title: "Hotel Raj Bagh Palace Jaipur",
+    image: "https://image.wedmegood.com/resized/450X/uploads/member/827689/1573028225_Screenshot_from_2019_11_06_13_43_54.png",
+  }
+ 
+];
 export default function PopularVenues() {
   return (
     <div>
@@ -8,20 +51,20 @@ export default function PopularVenues() {
         <h5 className="px-4 pt-4 text-xs">view all(10)</h5>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-5 2xl:grid-cols-8 gap-4 p-4">
-        {[1, 2, 3, 4, 5, 6, 7, 8].map((item) => (
-          <Card  key={item} className="md:w-[252px]">
+        {venues.map((item:any) => (
+          <Card  key={item.id} className="md:w-[252px]">
             <CardContent className="p-4 flex justify-center items-center">
               <img
-                width={150}
-                height={150}
-                src="https://via.placeholder.com/150"
+                width={450}
+                height={450}
+                src={item.image}
                 alt="media"
               />
             </CardContent>
             <CardFooter>
               <CardDescription>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
-                voluptates.
+            
+                {item.title}
               </CardDescription>
             </CardFooter>
           </Card>
